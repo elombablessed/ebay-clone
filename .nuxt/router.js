1d762b16b3a0972f3c81d38cb125ad26d53fb841
Elomba Blessed <blessedelomba@gmail.com>
2020-09-18 22:51:46 +0100

    cleaning up unwanted files

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,7 +3,6 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _f744c4a0 = () => interopDefault(import('../pages/inspire.vue' /* webpackChunkName: "pages/inspire" */))
 const _2b28cf28 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
@@ -23,10 +22,6 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
-    path: "/inspire",
-    component: _f744c4a0,
-    name: "inspire"
-  }, {
     path: "/",
     component: _2b28cf28,
     name: "index"
