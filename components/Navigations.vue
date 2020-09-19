ec9a298f4a2ef0ac84267c6d334533847519af7a
Elomba Blessed <blessedelomba@gmail.com>
2020-09-19 23:29:30 +0100

    Attempt finishing bottom navigatio or tool bar

--- components/Navigations.vue
+++ components/Navigations.vue
@@ -34,8 +34,11 @@
             <v-icon>mdi-chevron-down</v-icon>
         </div>
         <div class="search_input">
-            <v-icon>mdi-magnify</v-icon>
-            <input type="text" placeholder="Search for anything">
+            <div class="search_input_wrap">
+                <v-icon >mdi-magnify</v-icon>
+                <input type="text" placeholder="Search for anything">
+            </div>
+                
             <v-spacer></v-spacer>
             <div class="inner_search_input"> 
                 <span>All Categories</span>
@@ -51,11 +54,39 @@
     </div>
     <v-divider></v-divider>
     <div class="nav-bottom">
-        <p>bottom</p>
+        <div class="inner-nav-bottom">
+            <ul>
+                <li > <apan class="home"><a href="">Home</a> </apan></li>
+                <li> <v-icon small>mdi-heart</v-icon> Saved</li>
+                <li><a href="">Electronics</a> </li>
+                <li> <a href="">Fashion</a> </li>
+                <li> <a href="">Health & Beauty</a>  </li>
+                <li> <a href="">Motors</a> </li>
+                <li> <a href="">Collectibles</a> </li>
+                <li> <a href="">Sports</a> </li>
+                <li> <a href="">Home & Garden</a> </li>
+                <li> <a href="">Deals</a> </li>
+                <li> <a href="">Under $10</a> </li>
+            </ul>
+        </div>
+
     </div>
 </div>
 </template>
+
+<script>
+export default {
+    data:{
+        isHidden: false
+    }
+}
+</script>
+
 <style scoped>
+a{
+    text-decoration: none;
+    color: #555555;
+}
 .navigations{
     height: 25px;
     margin-top: 5px;
@@ -65,11 +96,15 @@
     align-items: center;
     justify-content: space-between; 
 }
+.my-2 > span{
+    color: #555555;
+}
 span{
     padding: 5px;
     font-family: Helvetica, sans-serif;
     font-size: 12px;
    justify-items: center;
+   
 }
 .navigations a{
     text-decoration: none;
@@ -105,6 +140,7 @@ span{
 }
 .shop span{
     display: grid;
+    color: #555555;
 }
 input{
     border-radius: 2px;
@@ -131,10 +167,15 @@ input{
     height: 45px;
     border-width: 1px;
 }
+.inner_search_input > span{
+    color: #555555;
+}
 .search_input > input{
-    
+    display: flex;
+    flex: 10;
     outline-width: 0;
     background-color: transparent;
+    font-family: Helvetica, sans-serif;
 
 }
 .my-2{
@@ -145,5 +186,31 @@ input{
     width: 168px;
    background-color:#3665f3;
    color: white;
+}
+.nav-bottom{
+    margin-left: 150px;
+    margin-right: 150px;
+    display: flex;
+    align-items: center;
+}
+.inner-nav-bottom{
+    align-content: space-between;
+    color: #555555;
+
+}
+.inner-nav-bottom > ul{
+    display: flex;
+    list-style: none;
+}
+li{
+    padding: 9px 12px;
+    
+}
+.home{
+    margin-top: 10px;
+    border-style: none none solid none;
+    border-width: 3px;
+   
+
 }
 </style>
