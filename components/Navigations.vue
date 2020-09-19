35f81944e88bd31527e124071ab3a132e485c596
Elomba Blessed <blessedelomba@gmail.com>
2020-09-19 02:27:22 +0100

    Adjusting CSS for header

--- components/Navigations.vue
+++ components/Navigations.vue
@@ -2,19 +2,17 @@
 <div>
     <div class="navigations">
         <div class="nav-top-left">
-            <span>Hi</span>
-            <span>Sign in</span>
-            <span>or</span>
-            <span>register</span>
-            <span>Daily Deals</span>
+            <span>Hi <a href="">Sign in </a> or <a href="">register</a> </span>
+            <span class="daily-deals">Daily Deals</span>
             <span>Help & Contact</span>
         </div>
-      <v-spacer></v-spacer>
+      
       <div class="nav-top-right">
             <span>Ship to</span>
             <span>Sell</span>
-            <span>Watchlist</span>
-            <span>My eBay</span>
+            <span>Watchlist<v-icon>mdi-chevron-down</v-icon></span>
+            
+            <span>My eBay<v-icon>mdi-chevron-down</v-icon></span>
         
 
       <v-btn icon>
@@ -27,16 +25,34 @@
     </div>
     </div>
     <v-divider></v-divider>
+    <div class="nav-middle">
+        <img src="https://pngimg.com/uploads/ebay/ebay_PNG22.png" alt="">
+        <v-icon>mdi-chevron-down</v-icon>
+        <p>Hey Middle</p>
+    </div>
+    <v-divider></v-divider>
+    <div class="nav-bottom">
+        <p>bottom</p>
+    </div>
 </div>
 </template>
 <style>
 .navigations{
+    height: 25px;
     margin-top: 5px;
     margin-left: 30px;
     margin-right: 30px;
     display: flex;
+    align-items: center;
     justify-content: space-between; 
-    justify-items: center;
+}
+.navigations span{
+    padding: 5px;
+    font-family: Helvetica, sans-serif;
+    font-size: 12px;
+}
+.navigations a{
+    text-decoration: none;
 }
 .nav-top-left{
     display: flex;
@@ -47,5 +63,11 @@
     display: flex;
     justify-content: space-evenly;
 }
-
+.daily-deals{
+    padding-left: 5px;
+}
+.nav-middle > img{
+     height: 70px;
+    
+}
 </style>
